956919d2329f7d48f3d03de269c3325bd69c25b8
ChinjuJose <73712484+ChinjuJose@users.noreply.github.com>
2021-10-03 11:32:16 +0530

    Add files via upload

---
+++ config.js
@@ -0,0 +1,10 @@
+export const firebaseConfig = {
+    apiKey: "AIzaSyBqiKgIHAXAjiOEHIRvr7sW1ZtasHvclm4",
+    authDomain: "storytellingapp-1e449.firebaseapp.com",
+    projectId: "storytellingapp-1e449",
+    storageBucket: "storytellingapp-1e449.appspot.com",
+    messagingSenderId: "202141035515",
+    appId: "1:202141035515:web:5fe3767030c7ebe87c795e",
+    measurementId: "G-563QQLQWY8"
+  };
+  
